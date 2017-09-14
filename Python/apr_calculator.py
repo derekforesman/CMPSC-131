b0f98ac27c3679cca44d2f7f8d71b64e68313961
MTAC <mtactheming@gmail.com>
2017-09-14 19:57:15 -0400

    Add files via upload

---
+++ Python/apr_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+deposit = float(input("What is the amount you will be depositing?\n")) # get the amount to be deposited
+apr = float(input("Please enter the APR for the account. For example (2) will be read as 2% or 0.02\n")) # get the percent APR
+years = int(input("How many years will it gain interest?\n")) # get the number of years
+interest = ((apr / 100) + 1) # calculate the interest. Will always be 1.XX unless interest is over 100%
+total = (interest** years) * deposit # calculate the final amount in the account
+print("Your final amount will be: $",total) # print the amount to the console
---
+++ Python/triangle_area.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3 
+
+height = int(input("Height: "))
+base = int(input("Base: "))
+triangle = (base * height) / 2
+print("The area of your triangle is: ",triangle)
